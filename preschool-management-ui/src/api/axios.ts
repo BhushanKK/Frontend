0527929309e020940081940e9d1f2f20ea352a06
Bhushan KK <bhushankachave9900@gmail.com>
2026-07-03 17:49:18 +0530

    moved url in .env file

--- preschool-management-ui/src/api/axios.ts
+++ preschool-management-ui/src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5202/api",
-  timeout: 30000,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: Number(import.meta.env.VITE_REQUEST_TIMEOUT),
   headers: {
     "Content-Type": "application/json",
   },
